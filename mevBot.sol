220f76c283b91aa1f8deff23d8ca29e99be9ade4
Eibrahim ayodeji <96651610+nonfungibleayo-eth@users.noreply.github.com>
2022-05-29 18:27:11 +0100

    Create mevBot.sol

---
+++ mevBot.sol
@@ -0,0 +1,13 @@
+//SPDX-License-Identifier: Unlicense
+pragma solidity >= 0.6.3 < 0.9.0;
+pragma abicoder v2;
+
+// import '@openzeppelin/contracts/token/ERC20/IERC20.sol';
+// import '@openzeppelin-contracts/contracts/token/ERC20/utils/SafeERC20.sol';
+// // import '@openzeppelin/contracts/access/Ownable.sol';
+// // import '@openzeppelin/contracts/utils/structs/EnumerableSet.sol';
+// // import 'hardhat/console.sol';
+
+import 'https://github.com/paco0x/amm-arbitrageur/blob/master/contracts/interfaces/IUniswapV2Callee.sol';
+import 'https://github.com/paco0x/amm-arbitrageur/blob/master/contracts/interfaces/IWETH.sol';
+import 'https://github.com/paco0x/amm-arbitrageur/blob/master/contracts/libraries/Decimal.sol';
