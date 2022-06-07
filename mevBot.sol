5a57221f5d8424c5a562a57bc0fbd88f6f6a6883
Eibrahim ayodeji <96651610+nonfungibleayo-eth@users.noreply.github.com>
2022-06-07 16:03:16 +0100

    Update mevBot.sol

--- mevBot.sol
+++ mevBot.sol
@@ -1,13 +1,63 @@
-//SPDX-License-Identifier: Unlicense
-pragma solidity >= 0.6.3 < 0.9.0;
-pragma abicoder v2;
+/// SPDX-License-Identifier: UNLICENSED;
+pragma solidity =0.6.6;
 
-// import '@openzeppelin/contracts/token/ERC20/IERC20.sol';
-// import '@openzeppelin-contracts/contracts/token/ERC20/utils/SafeERC20.sol';
-// // import '@openzeppelin/contracts/access/Ownable.sol';
-// // import '@openzeppelin/contracts/utils/structs/EnumerableSet.sol';
-// // import 'hardhat/console.sol';
+import '@uniswap/v2-periphery/contracts/libraries/UniswapV2Library.sol';
+import '@uniswap/v2-periphery/contracts/interfaces/IUniswapV2Router02.sol';
+import '@uniswap/v2-core/contracts/interfaces/IUniswapV2Pair.sol';
+import '@uniswap/v2-core/contracts/interfaces/IERC20.sol';
+import '@uniswap/v2-core/contracts/interfaces/IUniswapV2Factory.sol';
 
-import 'https://github.com/paco0x/amm-arbitrageur/blob/master/contracts/interfaces/IUniswapV2Callee.sol';
-import 'https://github.com/paco0x/amm-arbitrageur/blob/master/contracts/interfaces/IWETH.sol';
-import 'https://github.com/paco0x/amm-arbitrageur/blob/master/contracts/libraries/Decimal.sol';
+
+
+
+
+contract flashLoan {
+    address immutable factory;
+    uint constant deadline = 10 days;
+    IUniswapV2Router02 immutable sushirouter;
+
+    address private constant WETH = 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2;
+
+    constructor(address _factory, address _unirouter, address _sushirouter) public {
+        factory = _factory;
+        sushirouter = IUniswapV2Router02(_sushirouter);
+    }
+
+    function flashSwap(address _tokenBorrow, uint amount) external {
+        address pair = IUniswapV2Factory(factory).getPair(_tokenBorrow, WETH);
+        require(pair != address(0), 'not pair');
+
+        address token0 = IUniswapV2Pair(pair).token0();
+        address token1 = IUniswapV2Pair(pair).token1();
+
+        uint amount0 = _tokenBorrow == token0 ? amount : 0;
+        uint amount1 = _tokenBorrow == token1 ? amount : 0; 
+
+        IUniswapV2Pair(pair).swap(amount0, amount1, address(this), "");
+    }
+
+    function UniswapV2Call(address _sender, uint _amount0, uint _amount1, bytes calldata data) external {
+
+        address[] memory path = new address[] (2);
+        uint amountToken = _amount0 == 0 ? _amount1 : _amount0;
+
+        address token0 = IUniswapV2Pair(msg.sender).token0();
+        address token1 = IUniswapV2Pair(msg.sender).token1();
+
+        address pair = IUniswapV2Factory(factory).getPair(token0,token1);
+        require(msg.sender == pair, "Not Pair");
+
+        path[0] = _amount0 == 0 ? token1 : token0;
+        path[1] = _amount0 == 0 ? token0 : token1;
+
+        IERC20 token = IERC20(_amount0 == 0 ? token1 : token0);
+
+        token.approve(address(sushirouter), amountToken);
+
+        uint amountRequired = UniswapV2Library.getAmountsIn(factory, amountToken, path)[0];
+        uint amountRecieved = sushirouter.swapExactTokensForTokens(amountToken, amountRequired, path, msg.sender, deadline)[1];
+
+        
+
+    }
+}
